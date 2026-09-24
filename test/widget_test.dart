c21c1e16c8e4cee40fcb985db105a660c54cbd80
candradeveloper <candradeveloper@gmail.com>
2026-09-25 06:15:48 +0700

    fix: mock SharedPreferences in widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:emo_robot/main.dart';
 
 void main() {
+  setUp(() {
+    SharedPreferences.setMockInitialValues({});
+  });
+
   testWidgets('Aplikasi menampilkan judul Emo Robot', (WidgetTester tester) async {
     await tester.pumpWidget(const EmoRobotApp());
 
